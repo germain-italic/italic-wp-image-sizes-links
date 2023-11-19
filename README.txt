65cef69fc085fe3a64fd0e2cd99e268c82a9aedb
Germain Metti <germain@italic.fr>
2023-11-19 04:21:15 +0400

    Debug admin area

--- README.txt
+++ README.txt
@@ -3,7 +3,7 @@ Contributors: ashorlivs
 Tags: image, images, add_image_size
 Requires at least: 4.7
 Tested up to: 6.4.1
-Stable tag: 1.0.1
+Stable tag: 1.0.2
 Requires PHP: 7.4
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
@@ -39,6 +39,10 @@ __This is an **admin plugin**, it does not add anything to your theme or to publ
 
 
 == Changelog ==
+
+= v1.0.2 =
+
+* Debug admin area
 
 = v1.0.1 =
 
--- italic-image-sizes-links.php
+++ italic-image-sizes-links.php
@@ -1,10 +1,9 @@
 <?php
-
 /**
  * Plugin Name:       Italic WP Image Sizes Links
  * Plugin URI:        https://https://github.com/germain-italic/italic-wp-image-sizes-links
  * Description:       Display links to all available image sizes on the image edit page.
- * Version:           1.0.1
+ * Version:           1.0.2
  * Author:            Germain-Italic
  * Author URI:        https://https://www.italic.fr/
  * License:           GPL-2.0+
@@ -13,36 +12,57 @@
 */
 
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
+
+if (!defined('WPINC')) {
 	die;
 }
+
+
 
 add_filter('attachment_fields_to_edit', 'add_image_size_links', 10, 2);
 
 function add_image_size_links($form_fields, $post) {
-    $attachment_id = $post->ID;
-    $available_sizes = get_intermediate_image_sizes();
+    if (check_if_image_edit_page()) {
 
-    $size_links = '';
+        $attachment_id = $post->ID;
+        $available_sizes = get_intermediate_image_sizes();
 
-	echo '<div><ul>';
-    foreach ($available_sizes as $size) {
-        $image = wp_get_attachment_image_src($attachment_id, $size);
-        if ($image) {
-            $url = $image[0];
-            $width = $image[1];
-            $height = $image[2];
-            $size_links .= "<li><a href='$url' target='_blank'>$size ($width x $height)</a></li>";
+        $size_links = '';
+
+        echo '<div><ul>';
+        foreach ($available_sizes as $size) {
+            $image = wp_get_attachment_image_src($attachment_id, $size);
+            if ($image) {
+                $url = $image[0];
+                $width = $image[1];
+                $height = $image[2];
+                $size_links .= "<li><a href='$url' target='_blank'>$size ($width x $height)</a></li>";
+            }
         }
+        echo '</ul></div>';
+
+        $form_fields['image-size-links'] = array(
+            'label' => 'Image Sizes',
+            'input' => 'html',
+            'html'  => $size_links
+        );
+
+        return $form_fields;
     }
-	echo '</ul></div>';
+}
 
-    $form_fields['image-size-links'] = array(
-        'label' => 'Image Sizes',
-        'input' => 'html',
-        'html'  => $size_links
-    );
 
-    return $form_fields;
+
+function check_if_image_edit_page() {
+    global $pagenow, $typenow;
+
+    if ( ! is_admin() ) {
+        return false;
+    }
+
+    if ( 'post.php' === $pagenow && 'attachment' === $typenow ) {
+        return true;
+    }
+
+    return false;
 }
